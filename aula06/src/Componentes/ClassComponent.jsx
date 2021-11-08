b22ff141017f50766084617aba8d8e9e0bd46c0d
giovanispaula <giovannispaula@gmail.com>
2021-11-08 10:48:07 -0300

    Subiu aula06

--- aula06/src/Componentes/ClassComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-export default class ClassComponent extends Component {
-   render() {
-       return (
-           <li>SeuNome está convidado para festa</li>
-       )
-   }
-}
--- aula06/src/Componentes/FunctionComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
- 
-const functionComponent = () => {
-   return (
-       <li>SeuNome trará batata-frita</li>
-   )
-}
-export default functionComponent
